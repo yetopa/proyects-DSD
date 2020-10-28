5422f657e865d4ff68bcc35694f0a15118e2b7ea
yesmi.torres <yesmi.torres@qualifacts.com>
2020-10-28 00:56:27 -0500

    proyecto base

---
+++ src/main/java/pe/edu/upc/vacunacion/VacunacionApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.upc.vacunacion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VacunacionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VacunacionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pe/edu/upc/vacunacion/VacunacionApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.upc.vacunacion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VacunacionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
